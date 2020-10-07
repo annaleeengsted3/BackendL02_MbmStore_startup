b580e6be74bce631dede95ca32e8b110f2fa2fca
Annalee Engsted <annalee.engsted@hotmail.com>
2020-10-07 16:23:19 +0200

    Partial views and pagination implemented

--- MbmStore/Controllers/CatalogueController.cs
+++ MbmStore/Controllers/CatalogueController.cs
@@ -4,33 +4,42 @@ using System.Linq;
 using System.Threading.Tasks;
 using MbmStore.Models;
 using MbmStore.Infrastructure;
+using MbmStore.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MbmStore.Controllers
 {
     public class CatalogueController : Controller
     {
-        public IActionResult Index()
+        public int PageSize = 4;
+        // GET: Catalogue
+        public IActionResult Index(int page = 1)
         {
 
-            List<Book> Books = new List<Book>();
-            Books = Repository.Products.OfType<Book>().ToList();
 
-            List<MusicCD> CDs = new List<MusicCD>();
-            CDs = Repository.Products.OfType<MusicCD>().ToList();
+            ProductsListViewModel model = new ProductsListViewModel(); 
+            model = new ProductsListViewModel
+            {
+                //pagination:
+                Products = Repository.Products.OrderBy(p => p.ProductId).Skip((page - 1) * PageSize).Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page, 
+                    ItemsPerPage = PageSize, 
+                    TotalItems = Repository.Products.Count() }
+            };
+            return View(model);
 
-            List<Movie> Movies = new List<Movie>();
-            Movies = Repository.Products.OfType<Movie>().ToList();
-
-            ViewBag.Books = Books;
-            ViewBag.CDs = CDs;
-            ViewBag.Movies = Movies;
-
-    
+            //These changes pass a ProductsListViewModel object as the model data to the view.
 
 
 
-            return View(Repository.Products);
+
+
+
+
+
+
         }
     }
 }
---
+++ MbmStore/Models/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using MbmStore.Models;
+using System.Collections.Generic;
+namespace MbmStore.Models.ViewModels 
+{
+    public class ProductsListViewModel 
+    {
+        public IEnumerable<Product> Products { get; set; } 
+        public PagingInfo PagingInfo { get; set; } 
+    }
+}
